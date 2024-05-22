ed69446c003eea25b29829aafe2e8e5bb181b15f
Facundo Osre <fjosre@gmail.com>
2024-05-22 00:04:37 -0300

    varios arreglos intentando juntar lo de todos

--- js/head_foot.js
+++ js/head_foot.js
@@ -1,7 +1,8 @@
 let cabecera = `
     <nav class="navbar">
-        <h1 class="logo">CABAÑAS FANINA</h1>
-
+        <a href="index.html">
+            <h1 class="logo">CABAÑAS FANINA</h1>
+        </a>
         <label class="label_hamburguesa" for="menu_hamburguesa">
             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="list_icon" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
@@ -15,13 +16,10 @@ let cabecera = `
                 <a href="index.html" class="link">INICIO</a>
             </li>
             <li class="li_links">
-                <a href="reserva.html" class="link">RESERVAS</a>
-            </li>
-            <li class="li_links">
                 <a href="cabanas.html" class="link">CABAÑAS</a>
             </li>
             <li class="li_links">
-                <a href="ubicacion.html" class="link">UBICACION</a>
+                <a href="servicios.html" class="link">SERVICIOS</a>
             </li>
             <li class="li_links">
                 <a href="contacto.html" class="link">CONTACTO</a>
